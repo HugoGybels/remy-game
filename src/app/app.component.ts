0530ffddcd9f27afde775b6e3ff6bd0e6638cfdf
HugoGybels <hugo4242@hotmail.fr>
2020-09-11 18:06:52 +0200

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,30 @@ import { Ligne } from "./model/ligne.model";
 })
 export class AppComponent implements OnInit {
   public key: string;
-  public ligne: Ligne;
-  private touches: Touche[];
+  public lignes: Ligne[] = [];
 
   public ngOnInit(): void {
-    this.ligne = this.createLigne(1);
+    this.lignes.push(this.createLigne(1));
+    this.lignes.push(this.createLigne(2));
+    this.lignes.push(this.createLigne(4));
+    this.lignes.push(this.createLigne(3));
+
   }
 
   @HostListener("document:keypress", ["$event"])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.key = event.key;
+    let index = 0;
+    if (this.key === 'u') {
+      index = 1;
+    } else if (this.key === 'i') {
+      index = 2;
+    } else if (this.key === 'o') {
+      index = 3;
+    } else if (this.key === 'p') {
+      index = 4;
+    }
+    
   }
 
   private createLigne(idLigne: number): Ligne {
