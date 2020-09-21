0b04f64e9b4f37091e666c122ae6de8e17532fb1
HugoGybels <hugo4242@hotmail.fr>
2020-09-21 14:24:15 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { OnInit, Component, HostListener } from "@angular/core";
+import { StateEnum } from "./state.enum";
+
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
@@ -7,34 +9,51 @@ import { OnInit, Component, HostListener } from "@angular/core";
 export class AppComponent implements OnInit {
   public key: string;
   public lignes: number[] = [];
-  public started = false;
+  public stateEnum = StateEnum;
+  public state = StateEnum.SPLASH_SCREEN;
   public failed = false;
   public counter = 0;
 
-  private readonly nbLignes = 100;
-  private readonly keys = ['u', 'i', 'o', 'p'];
+  private readonly nbLignes = 10;
+  private readonly keys = ["u", "i", "o", "p"];
   private timerRef;
 
   public ngOnInit(): void {
     for (let i = 0; i < this.nbLignes; i++) {
       this.lignes.push(Math.floor(Math.random() * 4));
     }
-  }
+  } 
 
   @HostListener("document:keypress", ["$event"])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.key = event.key;
-    let hit = this.keys.indexOf(this.key);
-    if (hit > -1 && hit === this.lignes[0]) {
-      this.lignes.shift();
-      if(this.started === false) {
-        this.started = true;
-        const startTime = Date.now();
-        this.timerRef = setInterval(() => {
-          this.counter = Date.now() - startTime;
-        });
+
+    if (this.state === StateEnum.SPLASH_SCREEN) {
+      this.state = StateEnum.STARTED;
+      const startTime = Date.now();
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+      });
+    }
+
+    if (this.state === StateEnum.STARTED) {
+      let hit = this.keys.indexOf(this.key);
+      if (hit > -1) {
+        if (hit === this.lignes[0]) { 
+          this.lignes.shift();
+        } else {
+          this.state = StateEnum.FAILED;
+          this.end();
+        }
+      }
+      if (this.lignes.length === 0) {
+        this.state = StateEnum.ENDED;
+        this.end();
       }
     }
   }
 
+  private end() {
+    clearInterval(this.timerRef);
+  }
 }
---
+++ src/app/state.enum.ts
@@ -0,0 +1,6 @@
+export enum StateEnum {
+  SPLASH_SCREEN = 'SPLASH_SCREEN',
+  STARTED = 'STARTED',
+  FAILED = 'FAILED',
+  ENDED = 'ENDED'
+}
